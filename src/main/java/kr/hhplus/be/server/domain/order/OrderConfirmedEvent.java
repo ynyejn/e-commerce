9b43204f3df2aed9f4cd249af39bbcf3d687ab7d
ynyejn <yeounshi@gmail.com>
2025-02-14 04:38:01 +0900

    Feat : 데이터 플랫폼 전달 이벤트 적용

--- src/main/java/kr/hhplus/be/server/domain/order/OrderConfirmedEvent.java
+++ src/main/java/kr/hhplus/be/server/domain/order/OrderConfirmedEvent.java
@@ -3,15 +3,15 @@ package kr.hhplus.be.server.domain.order;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record OrderEvent(
+public record OrderConfirmedEvent(
         Long orderId,
         Long userId,
         Long paymentId,
         BigDecimal amount,
         LocalDateTime paidAt
 ) {
-    public static OrderEvent from(Order order) {
-        return new OrderEvent(
+    public static OrderConfirmedEvent from(Order order) {
+        return new OrderConfirmedEvent(
                 order.getId(),
                 order.getUser().getId(),
                 order.getId(),
--- src/main/java/kr/hhplus/be/server/interfaces/order/OrderEventListener.java
+++ src/main/java/kr/hhplus/be/server/interfaces/order/OrderEventListener.java
@@ -1,22 +1,23 @@
 package kr.hhplus.be.server.interfaces.order;
 
-import kr.hhplus.be.server.domain.order.OrderEvent;
+import kr.hhplus.be.server.domain.order.OrderConfirmedEvent;
 import kr.hhplus.be.server.infra.dataplatform.DataPlatformClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
-@Async
 @Slf4j
 @RequiredArgsConstructor
 public class OrderEventListener {
     private final DataPlatformClient dataPlatformClient;
 
-    @EventListener
-    public void handlePaymentEvent(OrderEvent event) {
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleOrderConfirmedEvent(OrderConfirmedEvent event) {
         log.info("Send to data platform - event: {}", event);
         dataPlatformClient.send(event.toString());
     }
